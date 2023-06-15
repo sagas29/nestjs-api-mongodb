9be0b7098bcc3a2534bb1427613db30b1e58a7f6
Jesus Sagastume <guada6190@gmai.com>
2023-06-15 17:09:26 -0600

    chore: rm env variables

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,9 +4,4 @@ export default () => ({
     user: process.env.BASIC_USERNAME,
     password: process.env.BASIC_PASSWORD,
   },
-  database: {
-    host: process.env.MONGODB_USER,
-    pass: process.env.MONGODB_PASSWORD,
-    port: parseInt(process.env.MONGODB_DOCKER_PORT, 10) || 5432,
-  },
 });
